821feabf8126d5df537268d7e2d45cda3335f0a1
doodream <rhomo17@naver.com>
2020-10-22 17:17:31 +0900

    Hash-1 solution 1

--- JavaAlogorithm/src/Hash_1.java
+++ JavaAlogorithm/src/Hash_1.java
@@ -1,28 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Hash_1 {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
-        // participant에 있는 String 변수를 하나씩 completion에 있는지 비교해본다.
-        for (int i = 0; i < participant.length; i++) {
-            boolean isInclude = false;
-            // completion에 participant에서 나온 name이 있는 지 확인
-            for (int j = 0; j < completion.length; j++) {
-                // 있다면 isInclude true
-                if (participant[i].equals(completion[j])) {
-                    completion[j] = null;
-                    isInclude = true;
-                    break;
-                }
-            }
-            // 만약 없었다면 없는 이름 반환.
-            if (!isInclude) {
-                answer = participant[i];
+        // participant 부분을 정렬하고
+        Arrays.sort(participant);
+        // completion 부분을 정렬한다.
+        Arrays.sort(completion);
+        // --> 문자열순으로 즉, 문자열 배열을 sort 함수로 정렬하면
+        // 알파벳, 모음순으로 정렬이된다.
+        // index를 빼주는 방법
+        int i;
+        // 완주자 수 만큼 비교한다.
+        for (i = 0; i < completion.length; i++) {
+            // 정렬된 과정에서 완주자는 참가자 의 한 부분이므로
+            // 정렬을 하면 참가자 한명씩 완주자 리스트를 비교해본다.
+            // 그럼 완주자리스트에 없는 참가자를 거를 수 있다.
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
-        return answer;
+        return participant[i];
     }
 }

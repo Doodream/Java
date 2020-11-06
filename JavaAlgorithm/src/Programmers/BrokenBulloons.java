c0bf4d3c674d84890a382e373e376a28c13e90bf
doodream <rhomo17@naver.com>
2020-11-06 21:15:53 +0900

    VERIFY Programmers - [Broken Bulloons] soltion

--- JavaAlgorithm/src/Programmers/BrokenBulloons.java
+++ JavaAlgorithm/src/Programmers/BrokenBulloons.java
@@ -1,55 +1,33 @@
 package Programmers;
 
-import java.util.ArrayList;
-
 class BrokenBulloons {
     public int solution(int[] a) {
-        int count = a.length;
+        int count = 0;
+
+        // 가장 작은 값 초기화
+        int minValue = Integer.MAX_VALUE;
+        // 선택된 풍선이 i 번째 일때 해당 풍선의 왼쪽의 그룹의 에서의 최솟값은 leftMin[i]
+        int[] leftMin = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (minValue > a[i]) minValue = a[i];
+            leftMin[i] = minValue;
+        }
+
+        // 선택된 풍선이 i 번째 일때 해당 풍선의 오른쪽의 그룹의 에서의 최솟값은 rightMin[i]
+        minValue = Integer.MAX_VALUE;
+        int[] rightMin = new int[a.length];
+        for (int j = a.length - 1; j >= 0 ; j--) {
+            if (minValue > a[j]) minValue = a[j];
+            rightMin[j] = minValue;
+        }
 
         for (int i = 0; i < a.length; i++) {
-            int bulloon = a[i];
-            int[] leftArr = new int[i];
-            for (int j = 0; j < i; j++) {
-                leftArr[j] = a[j];
-            }
-            int[] rightArr = new int[a.length - i - 1];
-            for (int j = i + 1; j < a.length; j++) {
-                rightArr[j - i - 1] = a[j];
-            }
-
-
-            ArrayList<Integer> minlist = new ArrayList<>();
-
-            if (leftArr.length == 0 || rightArr.length == 0) continue;
-            minlist.add(minBulloon(leftArr));
-            minlist.add(minBulloon(rightArr));
-            minlist.add(bulloon);
-
-            ArrayList<Integer> answerList = new ArrayList<>();
-            if (bulloon == maxBulloons(minlist)) {
-                answerList.add(bulloon);
-                System.out.print(bulloon+ " ");
-                count--;
-            }
+            // 왼쪽과 오른쪽 그룹의 최솟값 보다 크다면 넘어간다. // 최후 까지 남겨지지 못함
+            if(a[i] > rightMin[i] && a[i] > leftMin[i]) continue;
+            // 나머지는 추가
+            else count++;
         }
         return count;
-    }
-
-    //ArrayList의 최소값의 인덱스를 반환하는 함수
-    public int minBulloon(int[] arr) {
-        int num = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (num > arr[i]) num = arr[i];
-        }
-        return num;
-    }
-
-    public int maxBulloons(ArrayList<Integer> list) {
-        int num = list.get(0);
-        for (int i = 0; i < list.size(); i++) {
-            if (num < list.get(i)) num = list.get(i);
-        }
-        return num;
     }
 
     public static void main(String[] args) {
